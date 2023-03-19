28698847965b48d97c1098e8e4ed2fa7b0b1447c
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2023-03-19 14:52:56 -0300

    First Code in C#

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number = 15;
+            string Frase = "Este é o resultado da equação";
+            Console.WriteLine(Frase + " : " + (Number + 15));
+            Console.WriteLine("Pressione uma tecla para sair");
+            Console.ReadKey();
+        }
+    }
+}
